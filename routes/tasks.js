8d6fe04c83ec13bce9eaaa4af5a3630fd751bbb3
Lupita <lups1507@gmail.com>
2016-06-23 21:35:00 -0700

    ush ya

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,8 @@
+var database = require('../lib/tasks');
+var data = new database.Tasks();
 var express = require('express');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
@@ -11,5 +14,30 @@ router.get('/create', function(req, res, next) {
     res.render('create', { title: 'Create' });
 });
 
+/*GET show page. */
+router.get('/show', function(req, res, next) {
+    res.render('show', { title: 'Show' });
+});
+
+router.get('display/:opcion', function(req, res, next) {
+    var op = req.params.opcion;
+
+    res.send(op);
+});
+
+
+router.post('/create', function(req, res, next) {
+    var name = req.body.name;
+    var author = req.body.author;
+    var description = req.body.description;
+    var date = req.body.date;
+    var priority = req.body.priority;
+    var key = req.body.key;
+
+    var task_values = {'name':name, 'author': author,'description': description, 'date':date, 'priority':priority};
+
+    data.createTask(key,task_values)
+    res.render('create', { title: 'Create' });
+});
 
 module.exports = router;
