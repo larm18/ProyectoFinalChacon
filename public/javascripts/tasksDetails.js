1570f4189ec2d345203983b8465b472e0907aa23
Lupita <lups1507@gmail.com>
2016-07-28 21:38:54 -0700

    To do list test failed again :(

--- public/javascripts/tasksDetails.js
+++ public/javascripts/tasksDetails.js
@@ -17,12 +17,27 @@ var refresh_state = function(){
                 $("#task").empty();
             data.forEach(function (key) {
                 $("#tasks").append("<a href='../../tasks/getTask/"+ key + "'>" + key + "</a>")
-                if (status = 'Active') {
+                if (status == 'active') {
 
-                    status = 'active';
 
 
                     $("#tasks").append("<a href='../../tasks/change/" + status + "/done/"+key+"'> Done</a>")
+                    $("#tasks").append("<a href='../../tasks/change/" + status + "/pending/"+key+"'> Pending</a></br>")
+                }
+
+                if (status == 'pending') {
+
+
+
+                    $("#tasks").append("<a href='../../tasks/change/" + status + "/done/"+key+"'> Done</a>")
+                    $("#tasks").append("<a href='../../tasks/change/" + status + "/active/"+key+"'> Active</a></br>")
+                }
+
+                if (status == 'done') {
+
+
+
+                    $("#tasks").append("<a href='../../tasks/change/" + status + "/active/"+key+"'> Active</a>")
                     $("#tasks").append("<a href='../../tasks/change/" + status + "/pending/"+key+"'> Pending</a></br>")
                 }
             })
